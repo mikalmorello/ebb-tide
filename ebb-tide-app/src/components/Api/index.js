79d50c929da518ff0005f7c7f946c08165ccf625
Mikal Morello <mikalmorello@gmail.com>
2020-01-05 22:17:05 -0500

    DEV - Dynamically pass start and end date to fetch url

--- ebb-tide-app/src/components/Api/index.js
+++ ebb-tide-app/src/components/Api/index.js
@@ -2,24 +2,56 @@ import React from "react";
 
 
 // Api Call
-const fetchData = async station => {
-  const response = await fetch(
-    `https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&begin_date=20200101&end_date=20200102&datum=MLLW&station=${station}&time_zone=lst_ldt&units=english&interval=hilo&format=json`
-  );
+const fetchData = async (station, startDate, endDate) => {
+  const fetchUrl=`https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&begin_date=${startDate}&end_date=${endDate}&datum=MLLW&station=${station}&time_zone=lst_ldt&units=english&interval=hilo&format=json`
+  const response = await fetch(fetchUrl);
   const json = await response.json();
+  console.log(json);
   return json;
 };
+
+function formatDate(date) {
+  let newDate = date.toISOString().slice(0,10);
+  newDate = newDate.replace(/-/g,"");
+  return newDate;
+}
 
 
 function Api({station}) {
   const currentStation = station;
   const [tideData, setTideData] = React.useState();
+  const [startDate, setStartDate] = React.useState();
+  const [endDate, setEndDate] = React.useState();
   const [isLoading, setIsLoading] = React.useState(true);
+
+  
+  // Get Tide Date
+  let tideDate = '';
+  if(tideData){
+    tideDate = new Date(tideData.predictions[0].t);
+  }
+  
+  // Get Start Date
+//  React.useEffect(() => {
+//      const tempDate = new Date();
+//      let tempStartDate = `${tempDate.getFullYear()}${tempDate.getMonth()+1}${tempDate.getDate()}`;
+//      let tempEndDate = `${tempDate.getFullYear()}${tempDate.getMonth()+1}(${tempDate.getDate()}+1)`;
+//      setStartDate(tempStartDate);
+//      setEndDate(tempEndDate);
+//  });
+
   
   // On station change, call API
   React.useEffect(() => {
+      const dateToday = new Date();
+      const dateTomorrow = new Date(dateToday)
+        dateTomorrow.setDate(dateTomorrow.getDate() + 1);
+      let tempStartDate = formatDate(dateToday);
+      let tempEndDate = formatDate(dateTomorrow);
+//      setStartDate(tempStartDate);
+//      setEndDate(tempEndDate);
     (async () => {
-      const incomingData = await fetchData(station);
+      const incomingData = await fetchData(station, tempStartDate, tempEndDate);
       setTideData(incomingData);
       setIsLoading(false);
     })();
@@ -36,7 +68,8 @@ function Api({station}) {
   return (
     <div>
       Api station is {currentStation}
-      {tideData.predictions[0].t}
+      <br />
+      { tideDate.toUTCString() }
     </div>
   );
 }
