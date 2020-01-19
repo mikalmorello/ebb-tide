50f0ccdd0796b062ee53a98258076802d6535146
Mikal Morello <mikalmorello@gmail.com>
2020-01-18 22:31:35 -0500

    THEME - Wave tide progress theming

--- ebb-tide-app/src/components/Wave/index.js
+++ ebb-tide-app/src/components/Wave/index.js
@@ -1,13 +1,24 @@
 import React from "react";
+import TideDirectionIcon from '../../svg/TideDirectionIcon';
 import "./wave.scss";
 
 function Wave(){
   
   return(
     <section className="wave">
+      <div className="wave__header">
+        <div className="wave__header-unitheight">
+          <TideDirectionIcon /> 0.12<span className="wave__header-unit">ft</span>
+        </div>
+        <div className="wave__header-label">
+          High
+        </div>
+      </div>
       <div className="wave__container">
-          Wave
-        <div className=""></div>
+        <div className="percentage percentage-78"></div>
+      </div>
+      <div className="wave__footer">
+        Low
       </div>
     </section>
   )
---
+++ ebb-tide-app/src/svg/TideDirectionIcon.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function TideDirectionIcon(){
+	return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="23"
+      height="20"
+      viewBox="0 0 23 20"
+    >
+      <path
+        fill="none"
+        fillRule="evenodd"
+        stroke="#3D9DA3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M9.10024978 8.01383725L18.9605945 24.6 -0.760094959 24.6z"
+        transform="translate(-17 -259) translate(9 250) translate(10.4 2.7)"
+      ></path>
+    </svg>
+  );
+};
+
+export default TideDirectionIcon;
