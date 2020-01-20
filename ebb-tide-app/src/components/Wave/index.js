0a255137b9066297aad4418828e58d01c7992987
Mikal Morello <mikalmorello@gmail.com>
2020-01-19 22:04:06 -0500

    THEME - Add tide direction icon classes dynamically

--- ebb-tide-app/src/components/Wave/index.js
+++ ebb-tide-app/src/components/Wave/index.js
@@ -9,11 +9,23 @@ function Wave(){
   const nextTide = useSelector(appState => appState.api.nextTide);
   const isLoading = useSelector(appState => appState.api.isLoading);
   let tideHeight = 0;
+  let tideDirection = '';
   
+  // Format tide height 
   if(nextTide){
     tideHeight = Number(nextTide.v);
     tideHeight = tideHeight.toFixed(2);
     console.log(tideHeight);
+  }
+  
+  // Set class for tide direction 
+  function setTideClass(tideType) {
+      if(tideType === 'L') {
+        tideDirection = 'low';
+      } else if (tideType === 'H'){
+        tideDirection = 'high';
+      }
+      return tideDirection;
   }
   
   if (isLoading) {
@@ -27,7 +39,13 @@ function Wave(){
     <section className="wave">
       <div className="wave__header">
         <div className="wave__header-tide">
-          <TideDirectionIcon />{tideHeight}<span className="wave__header-unit">ft</span>
+          <div className={`wave__tide-direction ${setTideClass(nextTide.type)}`}>
+            <TideDirectionIcon />
+          </div>
+          <div className="wave__tide-height">
+            {tideHeight}
+          </div>
+          <span className="wave__header-unit">ft</span>
         </div>
         <div className="wave__header-label">
           High
